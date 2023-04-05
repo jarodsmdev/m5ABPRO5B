84fcd7a621a69ef8c55517ba1a161b764bffb063
Jarod Smith <lbriones.dev@gmail.com>
2023-04-04 20:32:43 -0400

    Config

--- src/java/modelo/Accidente.java
+++ src/java/modelo/Accidente.java
@@ -1,4 +1,3 @@
-
 package modelo;
 
 /**
@@ -6,5 +5,160 @@ package modelo;
  * @author Leonel Briones
  */
 public class Accidente {
-    
+
+    private int identificador;
+    private long rutCliente;
+    private String fecha;
+    private String hora;
+    private String lugar;
+    private String origen;
+    private String consecuencias;
+
+    /**
+     * Constructores de la clase.
+     */
+    public Accidente() {}
+
+
+    /**
+     *Constructor Parametrizado
+     * @param identificador: int
+     * @param cliente: String
+     * @param fecha: String
+     * @param hora: String
+     * @param lugar: String
+     * @param origen: String
+     * @param consecuencias: String
+     */
+    public Accidente(int identificador, Long rutCliente, String fecha, String hora, String lugar, String origen,
+            String consecuencias) {
+
+
+        this.identificador = identificador;
+        this.rutCliente = rutCliente;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.lugar = lugar;
+        this.origen = origen;
+        this.consecuencias = consecuencias;
+
+    }
+
+    /**
+     * Método Getter
+     * @return int
+     * identificador de tipo string.
+     */
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    /**
+     * Método Setter recibe int
+     * @param identificador 
+     */
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    /**
+     * Método Getter
+     * @return long
+     */
+    public long getRutCliente() {
+        return rutCliente;
+    }
+
+    /**
+     * Método Setter recibe long
+     * @param rutCliente 
+     */
+    public void setRutCliente(long rutCliente) {
+        this.rutCliente = rutCliente;
+    }
+
+    /**
+     * Método Getter
+     * @return String
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * Método Setter String
+     * @param fecha 
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * Método Getter
+     * @return String
+     */
+    public String getHora() {
+        return hora;
+    }
+
+    /**
+     * Método Setter
+     * @param hora 
+     */
+    public void setHora(String hora) {
+        this.hora = hora;  
+    }
+
+    /**
+     * Método Getter
+     * @return String
+     */
+    public String getLugar() {
+        return lugar;
+    }
+
+    /**
+     *Método Setter recibe un String
+     * @param lugar
+     */
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    /**
+     * Método Getter
+     * @return String
+     */
+    public String getOrigen() {
+        return origen;
+    }
+
+    /**
+     *Método Setter recibe String
+     * @param origen
+     */
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    /**
+     * Método Getter
+     * @return String
+     */
+    public String getConsecuencias() {
+        return consecuencias;
+    }
+
+    /**
+     * Método Setter recibe String
+     * @param consecuencias 
+     */
+    public void setConsecuencias(String consecuencias) {
+        this.consecuencias = consecuencias;
+    }
+
+    @Override
+    public String toString() {
+        return "Accidente ID: " + identificador + ", RUT: " + rutCliente + ", Fecha: " + fecha + ", Hora: " + hora + ", Lugar: " + lugar + ", Origen: " + origen + ", Consecuencias: " + consecuencias;
+    }
 }
--- src/java/modelo/Revision.java
+++ src/java/modelo/Revision.java
@@ -1,4 +1,3 @@
-
 package modelo;
 
 /**
@@ -6,5 +5,148 @@ package modelo;
  * @author Leonel Briones Palacios
  */
 public class Revision {
-    
+
+    private int idRevision;
+    private int idVisita;
+    private String nombreRevision;
+    private String detalles;
+    private int estado;
+
+    /**
+     * Constructor por defecto. idRevision es autogenerado
+     */
+    public Revision() {
+    }
+
+    /**
+     * Constructor parametrizado solicita todos los campos de clase
+     * @param idRevision
+     * @param nombreRevision
+     * @param idVisita
+     * @param detalles
+     * @param estado 
+     */
+    public Revision(int idRevision, String nombreRevision, int idVisita, String detalles, int estado) {
+
+        this.idRevision = idRevision;
+        this.idVisita = idVisita;
+        this.nombreRevision = nombreRevision;
+        this.detalles = detalles;
+        this.estado = estado;
+    }
+
+    /**
+     * Obtiene campo de clase idRevsiion tipo int
+     *
+     * @return método que Retorna el Id de la revisión
+     */
+    public int getIdRevision() {
+        return idRevision;
+    }
+
+    /**
+     * Establece campo de clase idRevision tipo int
+     *
+     * @param idRevision tipo int
+     */
+    public void setIdRevision(int idRevision) {
+        this.idRevision = idRevision;
+    }
+
+    /**
+     * Obtiene campo de clase idVisita tipo int
+     *
+     * @return int idVisita
+     */
+    public int getIdVisita() {
+        return idVisita;
+    }
+
+    /**
+     * Establece campo de clase idVisita tipo int
+     *
+     * @param idVisita tipo int
+     */
+    public void setIdVisita(int idVisita) {
+        this.idVisita = idVisita;
+    }
+
+    /**
+     * Obtiene campo de clase nombreRevision tipo String
+     * @return nombreRevision tipo String
+     */
+    public String getNombreRevision() {
+        return nombreRevision;
+    }
+
+    /**
+     * Establece campo de clase nombreRevision tipo String
+     * @param nombreRevision tipo String
+     */
+    public void setNombreRevision(String nombreRevision) {
+        this.nombreRevision = nombreRevision;
+    }
+
+    /**
+     * Obtiene campo de clase detalles tipo String
+     * @return detalles tipo String
+     */
+    public String getDetalles() {
+        return detalles;
+    }
+
+    /**
+     * Establece campo de clase detalles tipo String
+     * @param detalles tipo String
+     */
+    public void setDetalleRevisar(String detalles) {
+        this.detalles = detalles;
+    }
+
+    /**
+     * Obtiene campo de clase estado tipo int
+     * @return estado tipo int
+     */
+    public int getEstado() {
+        //1 (sin problemas), 2 (con observaciones), 3 (no aprueba),
+        return estado;
+    }
+
+    /**
+     * Establece campo de clase estado tipo int
+     * @param estado tipo int
+     */
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * Método que retorna String el resultado de una revisión, dependiendo del
+     * campo de clase estado que almacena enteros entre 1 a 3
+     * @return String con el resultado de la revisión
+     */
+    public String mostrarResultadoRevision() {
+        String resultado;
+
+        switch (getEstado()) {
+            case 1:
+                resultado = "SIN PROBLEMAS";
+                break;
+            case 2:
+                resultado = "CON OBSERVACIONES";
+                break;
+            case 3:
+                resultado = "NO APRUEBA";
+                break;
+            default:
+                resultado = "SE PRODUJO UN ERROR";
+        }
+        return resultado;
+    }
+
+    @Override
+    public String toString() {
+        return " [REVISIÓN] ID Revisión: " + idRevision + ", ID Visita: " + idVisita + ", Nombre Revisión: " + nombreRevision + ", Detalles: " + detalles + ", Estado: " + mostrarResultadoRevision() + ".";
+    }
+
 }
